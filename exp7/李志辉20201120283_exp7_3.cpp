1c966c655a0b026c41497b58cb2b185e832950bb
Lzh12580 <1316936100@qq.com>
2022-06-14 10:51:15 +0000

    增加7_3

--- exp7/李志辉20201120283_exp7_3.cpp
+++ exp7/李志辉20201120283_exp7_3.cpp
@@ -1,93 +1,52 @@
+#include<iostream>
+#include<math.h>
+#include<windows.h>
+#include<gl/glut.h>
 
-#include <GL/glut.h>
-#include <stdio.h>
-
-static float rotAngle = 0.;
-
-void init(void)
+// myInit
+void myInit()
 {
-	GLfloat values[2];
-	glGetFloatv(GL_LINE_WIDTH_GRANULARITY, values);
-	printf("GL_LINE_WIDTH_GRANULARITY value is %3.1f\n", values[0]);
-
-	glGetFloatv(GL_LINE_WIDTH_RANGE, values);
-	printf("GL_LINE_WIDTH_RANGE values are %3.1f %3.1f\n", values[0], values[1]);
-
-	glEnable(GL_LINE_SMOOTH);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glHint(GL_LINE_SMOOTH_HINT, GL_DONT_CARE);
-
-	glLineWidth(1.5);
-
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-}
-
-void display(void)
-{
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glColor3f(0.0, 1.0, 0.0);
-	glPushMatrix();
-	glRotatef(-rotAngle, 0.0, 0.0, 0.1);
-	glBegin(GL_LINES);
-	glVertex2f(-0.5, 0.5);
-	glVertex2f(0.5, -0.5);
-	glEnd();
-	glPopMatrix();
-
-	glColor3f(0.0, 0.0, 1.0);
-	glPushMatrix();
-	glRotatef(rotAngle, 0.0, 0.0, 0.1);
-	glBegin(GL_LINES);
-	glVertex2f(0.5, 0.5);
-	glVertex2f(-0.5, -0.5);
-	glEnd();
-	glPopMatrix();
-
-	glFlush();
-}
-
-void reshape(int w, int h)
-{
-	glViewport(0, 0, w, h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	if (w <= h)
-		gluOrtho2D(-1.0, 1.0, -1.0 * (GLfloat)h / (GLfloat)w, 1.0 * (GLfloat)h / (GLfloat)w);
-	else
-		gluOrtho2D(-1.0 * (GLfloat)w / (GLfloat)h, 1.0 * (GLfloat)w / (GLfloat)h, -1.0, 1.0);
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glColor3f(0.5f, 0.4f, 0.9f);
+	glPointSize(5.0);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
+	gluOrtho2D(0.0, 640, 0.0, 480);
 }
-
-void keyboard(unsigned char key, int x, int y)
+///myDisplay//
+void myDisplay()
 {
-	switch (key)
+	glBegin(GL_POINTS);
+	glVertex2i(1, 1);
+}
+/// myMouse///
+void myMouse(int button, int state, int x, int y)
+{
+	if (state == GLUT_DOWN)
 	{
-	case 'r': case 'R':
-		rotAngle += 20.;
-		if (rotAngle >= 360.0) rotAngle = 0.0;
-		glutPostRedisplay();
-		break;
-	case 27:
-		exit(0);
-		break;
-	default:
-		break;
+		glBegin(GL_POINTS);
+		glVertex2i(x, 480 - y);
+		glEnd();
+		glFlush();
+	}
+	else if (button == GLUT_RIGHT_BUTTON)
+	{
+		glClearColor(0.8, 0.6, 0.7, 0.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glFlush();
 	}
 }
-
-int main(int argc, char** argv)
+///main///
+void main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(400, 300);
+	glutInitWindowSize(640, 480);
+	glutInitWindowPosition(100, 150);
 	glutCreateWindow("李志辉20201120283");
-	init();
-	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboard);
-	glutDisplayFunc(display);
+
+	myInit();
+	glutDisplayFunc(myDisplay);
+	glutMouseFunc(myMouse);
 	glutMainLoop();
-	return 0;
 }
